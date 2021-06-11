b6a96f24fe518dc30cd972774f0eaebb04566b65
nobattery <lkg0705@naver.com>
2021-06-11 18:06:52 +0900

    add Google&Git Auth Login

--- src/service/firebase.js
+++ src/service/firebase.js
@@ -1,7 +1,13 @@
+import firebase from "firebase";
+
+//Firebase Sevice 추가.
+//통신에 필요한 key들은 .env파일에 저장
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
 };
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+export default firebaseApp;
